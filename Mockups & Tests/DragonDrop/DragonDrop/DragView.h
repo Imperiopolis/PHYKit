8c8fd42d682acf7dd973d5c74744d8fe6a4fafde
Nora Trapp <me@trappdesign.net>
2013-06-28 21:07:13 -0400

    DragonDrop uses PHYViews

--- Mockups & Tests/DragonDrop/DragonDrop/DragView.h
+++ Mockups & Tests/DragonDrop/DragonDrop/DragView.h
@@ -8,6 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface DragView : NSView
+@interface DragView : PHYView
 
 @end
--- Mockups & Tests/DragonDrop/DragonDrop/DragView.m
+++ Mockups & Tests/DragonDrop/DragonDrop/DragView.m
@@ -7,11 +7,10 @@
 //
 
 #import "DragView.h"
-#import <QuartzCore/QuartzCore.h>
 
 @interface DragView ()
 
-@property (strong, nonatomic) CALayer *testLayer;
+@property (strong, nonatomic) PHYView *testView;
 @property (nonatomic) NSPoint originalPoint;
 @property (nonatomic) BOOL clickedInView;
 
@@ -30,16 +29,6 @@
     return self;
 }
 
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
-    self = [super initWithCoder:aDecoder];
-    if (self)
-    {
-        [self doSetup];
-    }
-    return self;
-}
-
 - (void)awakeFromNib
 
 {
@@ -48,23 +37,17 @@
 
 - (void)doSetup
 {
-    self.layer = [CALayer layer];
-    [self setWantsLayer:YES];
-    self.testLayer = [CALayer layer];
-//    self.testLayer.anchorPoint = CGPointMake(0,0);
-    self.testLayer.frame = CGRectMake(0, 0, 100, 100);
-//    self.testLayer.bounds = CGRectMake(0, 0, 100, 100);
-//    self.testLayer.position = CGPointMake(50, 50);
-    self.testLayer.backgroundColor = [NSColor blueColor].CGColor;
-    [self.layer addSublayer: self.testLayer];
+    self.backgroundColor = [NSColor yellowColor];
+    self.testView = [[PHYView alloc] initWithFrame:NSMakeRect(0, 0, 100, 100)];
+    self.testView.backgroundColor = [NSColor blueColor];
+    [self addSubview: self.testView];
 }
 
 - (void)mouseDown:(NSEvent *)theEvent
 {
-    NSPoint point = theEvent.locationInWindow;
-    NSLog(@"%@", NSStringFromPoint(point));
+    NSPoint point = theEvent.locationInWindow;    
 
-    self.clickedInView = [self.testLayer hitTest:point] ? YES : NO;
+    self.clickedInView = [self.testView hitTest:point] ? YES : NO;
     
     self.originalPoint = theEvent.locationInWindow;
 }
@@ -72,13 +55,11 @@
 - (void)mouseDragged:(NSEvent*)theEvent
 {
     if (self.clickedInView) {
-        [CATransaction begin];
-        [CATransaction setDisableActions:YES];
-        CGPoint position = self.testLayer.position;
-        position.x += theEvent.deltaX;
-        position.y -= theEvent.deltaY;
-        self.testLayer.position = position;
-        [CATransaction commit];
+
+        NSRect frame = self.testView.frame;
+        frame.origin.x += theEvent.deltaX;
+        frame.origin.y -= theEvent.deltaY;
+        self.testView.frame = frame;
     }
 }
 
