119903bd8b0d09a1aee5f75dce6254fa5663af39
Zev Eisenberg <igeek1@gmail.com>
2013-07-09 20:22:51 -0400

    Add rope test project.

---
+++ Mockups & Tests/Rope/Rope/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rope
+//
+//  Created by Zev Eisenberg on 7/9/13.
+//  Copyright (c) 2013 Zev Eisenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
