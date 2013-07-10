3f57237ecfa135efba77b89430d46777737c25fd
Zev Eisenberg <igeek1@gmail.com>
2013-07-09 20:22:51 -0400

    Add rope test project.

---
+++ Mockups & Tests/Rope/Rope/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rope
+//
+//  Created by Zev Eisenberg on 7/9/13.
+//  Copyright (c) 2013 Zev Eisenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
