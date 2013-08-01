6c8fb9781d72608f8e3875269d8f5e66bc6e4b41
Zev Eisenberg <igeek1@gmail.com>
2013-07-31 20:10:15 -0400

    Initial setup of PHYWorld.

--- PHYKit/PHYKit/PHYWorld.h
+++ PHYKit/PHYKit/PHYWorld.h
@@ -7,57 +7,31 @@
 //
 
 #import <Foundation/NSObject.h>
-#import "CDStructures.h"
+
+typedef void(^PHYWorldBlock)(void);
 
 @class NSMutableArray;
+@protocol PHYDynamicItem;
 
-@interface PHYWorld : NSObject <NSCoding>
-{
-    struct b2World *_world;
-    struct b2Vec2 _gravity;
-    _Bool _doSleep;
-    double _accumulatedDt;
-    struct PHYContactListener _contactListener;
-    float _speed;
-    NSMutableArray *_bodies;
-    NSMutableArray *_joints;
-    NSMutableArray *_postStepBlocks;
-}
+@interface PHYWorld : NSObject
 
-+ (id)world;
-- (id).cxx_construct;
-- (void).cxx_destruct;
-- (BOOL)stepWithTime:(double)arg1 velocityIterations:(unsigned int)arg2 positionIterations:(unsigned int)arg3;
-- (void)removeAllJoints;
-- (void)removeJoint:(id)arg1;
-- (void)addJoint:(id)arg1;
+@property (nonatomic) struct CGPoint gravity;
+@property (nonatomic, readonly) NSArray *bodies;
+
++ (instancetype)world;
+- (void)stepWithTime:(NSTimeInterval)timeInterval velocityIterations:(NSUInteger)velocityIterations positionIterations:(NSUInteger)positionIterations;
+
 - (void)removeAllBodies;
-- (void)removeBody:(id)arg1;
-- (void)addBody:(id)arg1;
-- (id)bodies;
-@property(nonatomic) id <PHYContactDelegate> contactDelegate;
-- (id)collisionDelegate;
-- (void)setCollisionDelegate:(id)arg1;
-@property(nonatomic) struct CGPoint gravity;
+- (void)removeBody:(id<PHYDynamicItem>)body;
+- (void)addBody:(id<PHYDynamicItem>)body;
 - (BOOL)hasBodies;
-- (id)copy;
-- (id)copyWithZone:(struct _NSZone *)arg1;
-- (void)dealloc;
-- (id)init;
-- (id)initWithCoder:(id)arg1;
-- (void)encodeWithCoder:(id)arg1;
-@property(nonatomic) float speed;
-- (id)bodyAlongRayStart:(struct CGPoint)arg1 end:(struct CGPoint)arg2;
-- (id)bodyInRect:(struct CGRect)arg1;
-- (id)bodyAtPoint:(struct CGPoint)arg1;
-- (void)enumerateBodiesAlongRayStart:(struct CGPoint)arg1 end:(struct CGPoint)arg2 usingBlock:(id)arg3;
-- (void)enumerateBodiesInRect:(struct CGRect)arg1 usingBlock:(id)arg2;
-- (void)enumerateBodiesAtPoint:(struct CGPoint)arg1 usingBlock:(id)arg2;
-- (void)_runBlockOutsideOfTimeStep:(id)arg1;
-@property(nonatomic) _Bool _doSleep;
-@property(retain, nonatomic) NSMutableArray *_bodies;
-@property(nonatomic) struct b2Vec2 _gravity;
-@property(nonatomic) struct b2World *_world;
+
+- (id)bodyAlongRayStart:(struct CGPoint)startPoint end:(struct CGPoint)endPoint;
+- (id)bodyInRect:(struct CGRect)rect;
+- (id)bodyAtPoint:(struct CGPoint)point;
+- (void)enumerateBodiesAlongRayStart:(struct CGPoint)startPoint end:(struct CGPoint)endPoint usingBlock:(PHYWorldBlock)block;
+- (void)enumerateBodiesInRect:(struct CGRect)rect usingBlock:(PHYWorldBlock)block;
+- (void)enumerateBodiesAtPoint:(struct CGPoint)point usingBlock:(PHYWorldBlock)block;
 
 @end
 
--- PHYKit/PHYKit/PHYWorld.m
+++ PHYKit/PHYKit/PHYWorld.m
@@ -6,4 +6,170 @@
 //  Copyright (c) 2013 Nora Trapp. All rights reserved.
 //
 
-#include <PHYWorld.h>
+#import <PHYWorld.h>
+#import <Box2D/Box2D.h>
+#import "PHYDynamicBehavior.h"
+
+#define kPixelToMeterRatio 16
+
+@interface PHYWorld ()
+{
+    b2World *_b2world;
+}
+
+@property (strong, nonatomic) NSMutableArray *mutableBodies;
+
+@end
+
+@implementation PHYWorld
+
++ (instancetype)world
+{
+    return [[PHYWorld alloc] init];
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        b2Vec2 gravity;
+        gravity.SetZero();
+        _b2world = new b2World(gravity);
+        
+        _b2world->SetContinuousPhysics(true);
+        
+        self.mutableBodies = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+- (void)stepWithTime:(NSTimeInterval)timeInterval
+{
+    // step with default iterations
+    [self stepWithTime:timeInterval velocityIterations:8 positionIterations:1];
+}
+
+- (void)stepWithTime:(NSTimeInterval)timeInterval velocityIterations:(NSUInteger)velocityIterations positionIterations:(NSUInteger)positionIterations
+{
+    _b2world->Step(timeInterval, (int32)velocityIterations, (int32)positionIterations);
+    
+    //Iterate over the bodies in the physics world
+	for (b2Body* b = _b2world->GetBodyList(); b; b = b->GetNext())
+	{
+		if (b->GetUserData() != NULL)
+		{
+			id<PHYDynamicItem> dynamicItem = (__bridge id<PHYDynamicItem>)b->GetUserData();
+            
+			// y Position subtracted because of flipped coordinate system
+			CGPoint newCenter = CGPointMake(b->GetPosition().x * kPixelToMeterRatio,
+                                            b->GetPosition().y * kPixelToMeterRatio);
+			dynamicItem.center = newCenter;
+            
+#warning make sure the minus sign is supposed to be there, and look here if we are getting backwards rotations
+			CGAffineTransform transform = CGAffineTransformMakeRotation(- b->GetAngle());
+            
+			dynamicItem.transform = transform;
+		}
+	}
+
+}
+
+
+- (void)removeAllBodies
+{
+    for (b2Body* body = _b2world->GetBodyList(); body; body = body->GetNext())
+	{
+        _b2world->DestroyBody(body);
+	}
+    
+    [self.mutableBodies removeAllObjects];
+}
+
+- (void)removeBody:(id<PHYDynamicItem>)dynamicItem
+{
+#warning need a mapping table so we can look up a b2body, given a dynamic item
+}
+
+- (void)addBody:(id<PHYDynamicItem>)dynamicItem
+{
+    // Define the dynamic body.
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+    
+	CGPoint p = dynamicItem.center;
+	CGSize boxSize = CGSizeMake(dynamicItem.bounds.size.width / kPixelToMeterRatio / 2.0,
+                                dynamicItem.bounds.size.height / kPixelToMeterRatio / 2.0);
+    
+	bodyDef.position.Set(p.x / kPixelToMeterRatio,
+                         p.y / kPixelToMeterRatio);
+    
+	bodyDef.userData = (__bridge void *)dynamicItem;
+    
+	// Tell the physics world to create the body
+	b2Body *body = _b2world->CreateBody(&bodyDef);
+    
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+    
+	dynamicBox.SetAsBox(boxSize.width, boxSize.height);
+    
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 3.0f;
+	fixtureDef.friction = 0.3f;
+	fixtureDef.restitution = 0.5f; // 0 is a lead ball, 1 is a super bouncy ball
+	body->CreateFixture(&fixtureDef);
+    
+	// a dynamic body reacts to forces right away
+	body->SetType(b2_dynamicBody);
+    
+    [self.mutableBodies addObject:dynamicItem];
+}
+
+- (NSArray *)bodies
+{
+    return [NSArray arrayWithArray:self.mutableBodies];
+}
+
+- (BOOL)hasBodies
+{
+    return [self.bodies count] != 0;
+}
+
+
+- (id)bodyAlongRayStart:(struct CGPoint)startPoint end:(struct CGPoint)endPoint
+{
+    return nil;
+}
+
+- (id)bodyInRect:(struct CGRect)rect
+{
+    return nil;
+}
+
+- (id)bodyAtPoint:(struct CGPoint)point
+{
+    return nil;
+}
+
+- (void)enumerateBodiesAlongRayStart:(struct CGPoint)startPoint end:(struct CGPoint)endPoint usingBlock:(PHYWorldBlock)block
+{
+    
+}
+
+- (void)enumerateBodiesInRect:(struct CGRect)rect usingBlock:(PHYWorldBlock)block
+{
+    
+}
+
+- (void)enumerateBodiesAtPoint:(struct CGPoint)point usingBlock:(PHYWorldBlock)block
+{
+    
+}
+
+
+
+@end
